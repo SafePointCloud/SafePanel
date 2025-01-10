7f84c497a1438914263336e3451607a353c24b4a
monster <monster@monsterdeMBP.hz.chaitin.net>
2025-01-10 17:06:28 +0800

    fisrt bpf demo

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "github.com/google/gopacket"
+    "github.com/google/gopacket/pcap"
+    "github.com/google/gopacket/layers"
+)
+
+
+func main() {
+    handle, err := pcap.OpenLive("en0", 65536, true, pcap.BlockForever)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer handle.Close()
+
+    err = handle.SetBPFFilter("tcp")
+    if err!= nil {
+        fmt.Println(err)
+        return
+    }
+
+    packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		ipLayer := packet.Layer(layers.LayerTypeIPv4)
+		if ipLayer != nil {
+			ip, _ := ipLayer.(*layers.IPv4)
+            fmt.Println(ip.SrcIP)
+		}
+	}
+}
